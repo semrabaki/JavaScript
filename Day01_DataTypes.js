a2f91e7f6b362b6888bfcfdde194691c23ac2251
Semra Baki <usemrabaki@gmail.com>
2022-02-03 13:40:26 -0600

    first commit

---
+++ Day01_DataTypes.js
@@ -0,0 +1,60 @@
+//console.clear();
+/*
+Primitive Data Types JS
+string, number, boolean, null, undefined
+*/
+alert("Gives us warnig from top of window");
+var myNum=37;
+console.log(myNum);//37
+console.log(typeof myNum);//number
+
+var myNum=37.7;
+console.log(myNum);//37.7
+console.log(typeof myNum);//number
+
+var myNum=20/0;
+console.log(myNum);//infinity
+console.log(typeof myNum);//number
+
+var myNum=20+10;
+console.log(myNum);//30
+console.log(typeof myNum);//number
+
+
+var myName='java script';
+console.log(myName); //java script
+console.log(typeof myName);//string
+
+var myName='java';
+console.log(myName); //java
+console.log(typeof myName);//string
+
+console.clear();
+
+var num1=20+'Tugra'; //20Tugra
+console.log(num1);
+console.log(typeof num1);//string
+
+var num1='Tugra'+20; //Tugra20
+console.log(num1);
+console.log(typeof num1);//string
+
+console.clear();
+var isExist=true;
+console.log(isExist);//true
+console.log(typeof isExist);//boolean
+
+
+var isExist=false;
+console.log(isExist);//false
+console.log(typeof isExist);//boolean
+
+var selectedCar;
+console.log(selectedCar);//undefined
+console.log(typeof selectedCar);//undefined
+
+var pickedColor=null;
+console.log(pickedColor);//null
+console.log(typeof pickedColor);//object
+
+
---
+++ Day01_Variables.js
@@ -0,0 +1,26 @@
+console.log("Hello World!")
+
+var myName;
+console.log(myName); //endefined
+
+//var variables can  be declared multiple times
+myName="Semra";
+console.log(myName);//Semra
+
+var myName="JavaScript";
+
+console.log(myName);//Java Script
+console.log(typeof myName);//String
+
+myName=true;
+console.log(typeof myName);//true
+
+myName=37
+console.log(typeof myName);//number
+
+var fName="Orhan",lName="Tugra";
+console.log(fName);
+console.log(lName);
+console.log(fName + lName);
+console.log(fName + " " +lName);
+
